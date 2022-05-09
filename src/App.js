fd6b61d4289be8d4ae5b9b95e5979a16aaa0e78b
Amr Desouky <desoukya@gmail.com>
2022-05-09 15:36:33 -0400

    poc

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import FlatfileImporter from "@flatfile/adapter";
+import "bootstrap/dist/css/bootstrap.css";
+import Button from "react-bootstrap/Button";
+import Form from 'react-bootstrap/Form'
+import FloatingLabel from 'react-bootstrap/FloatingLabel'
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+import Tooltip from "react-bootstrap/Tooltip";
+
+const LICENSE_KEY = "70cbe3fa-b6ba-4943-be91-c2b93060c496";
+const importer = new FlatfileImporter(LICENSE_KEY, {
+  type: "imports",
+  fields: [
+    { key: "City", label: "City" },
+    { key: "State", label: "State" }
+  ],
+  managed: true,
+});
 
 function App() {
+  const [results, setResults] = useState('Your output will appear here');
+
+  const renderTooltip = props => (
+    <Tooltip {...props}>Upload your CSV</Tooltip>
+  );
+  const launchFlatFile = async () => {
+    const userData = await importer.requestDataFromUser();
+    console.log('userData :>> ', userData);
+    importer.displayLoader();
+    setTimeout(() => {
+      importer.displaySuccess("CSV File Successfully Uploaded!");
+      setResults(JSON.stringify(userData.validData, null, 2));
+    }, 1500);
+  };
+
+  useEffect(() => {
+    importer.setCustomer({
+      userId: "1",
+      name: "Amr Desouky"
+    });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <OverlayTrigger placement="top" overlay={renderTooltip}>
+        <Button onClick={launchFlatFile}>Upload CSV</Button>
+      </OverlayTrigger>
+      <FloatingLabel controlId="floatingTextarea" label={results} className="mb-3">
+        <Form.Control as="textarea" />
+      </FloatingLabel>
     </div>
   );
 }
